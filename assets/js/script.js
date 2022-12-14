d1a93b9be77cad644750e17bc6cf1d5cce1680b6
Minhaj06 <milonmdminhajkobir@gmail.com>
2022-12-14 23:44:57 +0600

    Working On Shop Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,20 @@ var swiper2 = new Swiper(".mySwiper2", {
     swiper: swiper,
   },
 });
+
+// Range Slider
+let rangeSlider = document.querySelector(".range-slider");
+let rangeSliderAfter = window.getComputedStyle(rangeSlider, "::after");
+
+let rangeSelector = document.querySelector(".range-selector");
+let rangeSelectedValue = document.querySelector(".range-selected-value");
+
+rangeSelectedValue.innerHTML = rangeSlider.value * 100 + "৳";
+
+rangeSlider.oninput = function () {
+  rangeSelector.style.left = this.value + "%";
+
+  rangeSelectedValue.innerHTML = this.value * 100 + "৳";
+
+  rangeSlider.style.setProperty("--rangeSliderWidth", this.value + "%");
+};
